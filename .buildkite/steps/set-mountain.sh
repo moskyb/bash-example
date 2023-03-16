4afd2a8cba3b4a12c3330131962dc1134af853a0
Ben Moskovitz <ben.m@buildkite.com>
2023-03-16 16:04:54 +1300

    test job api

--- .buildkite/steps/set-mountain.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-curl --unix-socket "$BUILDKITE_AGENT_JOB_API_SOCKET" \
-  -H "Authorization: Bearer $BUILDKITE_AGENT_JOB_API_TOKEN" \
-  -d '{ "env": { "MOUNTAIN": "cotopaxi" } } }' \
-  -XPATCH http://localhost/api/current-job/v0/env | jq .
--- .buildkite/steps/test_mounted_network_drive_access.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-mounted_directory="$HOME/Desktop"
-
-echo "--- Checking access to $mounted_directory"
-
-ls -al "$mounted_directory"
