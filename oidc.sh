8aaee3a1a271211810871244c4436a5faadc26a6
Ben Moskovitz <ben.m@buildkite.com>
2024-09-20 13:35:44 +1000

    oidc

---
+++ oidc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+token=$(buildkite-agent oidc request-token --audience "https://packages.buildkite.com/bennos-super-secret-cool-time-org/alpine" --claim "queue_key")
+
+curl -v -H "Authorization: Bearer $token" https://api.buildkite.com/v2/packages/organizations/bennos-super-secret-cool-time-org/registries/alpine/packages/019075f2-ae48-7682-98e3-1edcc6430739 | jq
