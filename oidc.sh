4426402a067bcd83f2e4bcc6966d83aa50f3a3a3
Ben Moskovitz <ben.m@buildkite.com>
2025-03-27 15:34:23 +1100

    delete mode

--- oidc.sh
+++ oidc.sh
@@ -5,4 +5,4 @@ echo "Token: $token"
 echo
 echo
 
-curl --fail-with-body -H "Authorization: Bearer $token" https://api.buildkite.com/v2/packages/organizations/bennos-super-secret-cool-time-org/registries/alpine/packages/019075f2-ae48-7682-98e3-1edcc6430739 | jq
+curl -X DELETE --fail-with-body -H "Authorization: Bearer $token" https://api.buildkite.com/v2/packages/organizations/bennos-super-secret-cool-time-org/registries/alpine/packages/019075f2-ae48-7682-98e3-1edcc6430739 | jq
