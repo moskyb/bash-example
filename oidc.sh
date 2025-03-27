8e9294fb545dbb0f257f44675df7d778699dcfe1
Ben Moskovitz <ben.m@buildkite.com>
2025-03-27 15:34:23 +1100

    delete mode

--- oidc.sh
+++ oidc.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-token=$(buildkite-agent oidc request-token --audience "https://packages.buildkite.com/bennos-super-secret-cool-time-org/alpine" --claim "queue_key")
-echo "Token: $token"
-echo
-echo
+set -x
 
-curl --fail-with-body -H "Authorization: Bearer $token" https://api.buildkite.com/v2/packages/organizations/bennos-super-secret-cool-time-org/registries/alpine/packages/019075f2-ae48-7682-98e3-1edcc6430739 | jq
+org_slug="test"
+registry_slug="sneke"
+
+token=$(buildkite-agent oidc request-token \
+  --audience "http://packages.buildkite.localhost/$org_slug/$registry_slug" \
+  --claim "queue_key")
+
+curl -X GET --fail-with-body -H "Authorization: Bearer $token" \
+ "http://api.buildkite.localhost/v2/packages/organizations/$org_slug/registries/$registry_slug/packages" | jq
